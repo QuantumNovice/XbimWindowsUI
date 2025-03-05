69db1fa59042b64658f9642a7b7ee2641c3dc556
QuantumNovice <portabl3lapy@gmail.com>
2025-03-05 16:06:16 +0900

    fixed deprecation issues with DotNetZip by Switching to SharpZipLib

--- XbimXplorer/PluginSystem/IPackageExtensions.cs
+++ XbimXplorer/PluginSystem/IPackageExtensions.cs
@@ -1,45 +1,57 @@
 ﻿using System.IO;
 using NuGet;
-using Ionic.Zip;
+using ICSharpCode.SharpZipLib.Zip;
+using System;
+
 
 namespace XbimXplorer.PluginSystem
 {
     internal static class PackageExtensions
     {
-        /// <summary>
-        /// Extract the manifest file to the specified file name
-        /// </summary>
-        /// <param name="package"></param>
-        /// <param name="targetFileName"></param>
-        /// <returns>true if successful, false if fail.</returns>
-        internal static bool ExtractManifestFile(this IPackage package, string targetFileName)
-        {
+		/// <summary>
+		/// Extract the manifest file to the specified file name
+		/// </summary>
+		/// <param name="package"></param>
+		/// <param name="targetFileName"></param>
+		/// <returns>true if successful, false if fail.</returns>
+		internal static bool ExtractManifestFile(this IPackage package, string targetFileName)
+		{
+			using (var fs = package.GetStream())
+			using (var zipFile = new ZipFile(fs))
+			{
+				foreach (ZipEntry entry in zipFile)
+				{
+					// Skip directories (only process files)
+					if (!entry.IsFile)
+						continue;
 
-            using (var fs = package.GetStream())
-            {
+					// Check for .nuspec extension (case-insensitive)
+					string extension = Path.GetExtension(entry.Name);
+					if (extension == null || !extension.Equals(".nuspec", StringComparison.OrdinalIgnoreCase))
+						continue;
 
-                using (var zf = ZipFile.Read(fs))
-                {
-                    foreach (ZipEntry zipEntry in zf)
-                    {
-                        if (zipEntry.IsDirectory)
-                        {
-                            continue; // Ignore directories
-                        }
-                        var entryFileName = zipEntry.FileName;
-                        var extension = Path.GetExtension(entryFileName);
-                        if (extension == null || extension.ToLowerInvariant() != ".nuspec")
-                            continue;
+					// Ensure the target directory exists
+					string targetDirectory = Path.GetDirectoryName(targetFileName);
+					if (!Directory.Exists(targetDirectory))
+					{
+						Directory.CreateDirectory(targetDirectory);
+					}
 
-                        zipEntry.FileName = Path.GetFileName(targetFileName);
-                        zipEntry.Extract(Path.GetDirectoryName(targetFileName), ExtractExistingFileAction.OverwriteSilently);
+					// Build the output file path using the target file's name
+					string outputFilePath = Path.Combine(targetDirectory, Path.GetFileName(targetFileName));
 
-                        return true;
-                    }
-                }
-            }
+					// Extract the entry by copying its stream to the output file
+					using (var entryStream = zipFile.GetInputStream(entry))
+					using (var outputStream = File.Create(outputFilePath))
+					{
+						entryStream.CopyTo(outputStream);
+					}
 
-            return false;
-        }
-    }
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
 }
--- XplorerPlugin.Bcf/BCFFile.cs
+++ XplorerPlugin.Bcf/BCFFile.cs
@@ -1,4 +1,5 @@
-﻿using Ionic.Zip;
+﻿using ICSharpCode.SharpZipLib.Zip;
+
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -8,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace Xbim.BCF
 {
@@ -20,79 +22,145 @@ namespace Xbim.BCF
         private const string SnapshotFileName = "snapshot.png";
 
 
-        public void LoadFile(string fileName)
-        {
-            // BCFFile retFile = new BCFFile();
-            using (ZipFile z = ZipFile.Read(fileName))
-            {
-                Regex r = new Regex(@"(?<guid>.*?)/(?<fname>.*)");
-                foreach (var zipentry in z)
-                {
-                    string tFName = System.IO.Path.GetTempFileName();
-                    var res = r.Match(zipentry.FileName);
-                    if (res.Success)
-                    {
-                        using (BinaryWriter writer = new BinaryWriter(File.Open(tFName, FileMode.Create)))
-                        {
-                            zipentry.Extract(writer.BaseStream);
-                        }
+		public void LoadFile(string fileName)
+		{
+			// Compile the regex pattern to extract the GUID and file name.
+			Regex regex = new Regex(@"(?<guid>.*?)/(?<fname>.*)", RegexOptions.Compiled);
 
-                        string guid = res.Groups["guid"].Value;
-                        string fname = res.Groups["fname"].Value;
-                        if (!Instances.Where(x=>x.Markup.Topic.Guid == guid).Any())
-                            Instances.Add(new BcfInstance(guid));
+			// Open the ZIP file for reading.
+			using (FileStream fs = File.OpenRead(fileName))
+			using (ZipFile zipFile = new ZipFile(fs))
+			{
+				foreach (ZipEntry entry in zipFile)
+				{
+					// Create a temporary file to extract the entry.
+					string tempFilePath = Path.GetTempFileName();
 
-                        BcfInstance inst = Instances.Where(x => x.Markup.Topic.Guid == guid).FirstOrDefault();
-                        switch (fname.ToLowerInvariant())
-                        {
-                            case MarkupFileName:
-                                inst.Markup = Markup.LoadFromFile(tFName);
-                                break;
-                            case ViewpointFileName:
-                                inst.VisualizationInfo = VisualizationInfo.LoadFromFile(tFName);
-                                break;
-                            case SnapshotFileName:
-                                var bi = new System.Windows.Media.Imaging.BitmapImage();
-                                bi.BeginInit();
-                                bi.CacheOption = System.Windows.Media.Imaging.BitmapCacheOption.OnLoad;
-                                bi.UriSource = new Uri(tFName);
-                                bi.EndInit();
-                                inst.SnapShot = bi;
-                                break;
-                            default:
-                                break;
-                        }
-                        File.Delete(tFName);
-                    }
-                }    
-            }
-        }
+					// Try matching the entry name against the expected pattern.
+					var match = regex.Match(entry.Name);
+					if (!match.Success)
+						continue;
 
-        private string GetTemporaryDirectory(string guid)
+					// Extract the entry to the temporary file.
+					using (Stream entryStream = zipFile.GetInputStream(entry))
+					using (FileStream tempFileStream = File.OpenWrite(tempFilePath))
+					{
+						entryStream.CopyTo(tempFileStream);
+					}
+
+					// Retrieve the GUID and inner file name from the match groups.
+					string guid = match.Groups["guid"].Value;
+					string innerFileName = match.Groups["fname"].Value;
+
+					// If no instance exists with this GUID, create a new one.
+					if (!Instances.Any(x => x.Markup.Topic.Guid == guid))
+					{
+						Instances.Add(new BcfInstance(guid));
+					}
+
+					// Retrieve the instance for the given GUID.
+					BcfInstance instance = Instances.First(x => x.Markup.Topic.Guid == guid);
+
+					// Process the temporary file based on its name (case-insensitive).
+					switch (innerFileName.ToLowerInvariant())
+					{
+						case MarkupFileName:
+							instance.Markup = Markup.LoadFromFile(tempFilePath);
+							break;
+						case ViewpointFileName:
+							instance.VisualizationInfo = VisualizationInfo.LoadFromFile(tempFilePath);
+							break;
+						case SnapshotFileName:
+							var bitmap = new BitmapImage();
+							bitmap.BeginInit();
+							bitmap.CacheOption = BitmapCacheOption.OnLoad;
+							bitmap.UriSource = new Uri(tempFilePath);
+							bitmap.EndInit();
+							instance.SnapShot = bitmap;
+							break;
+						default:
+							// Unknown file type – ignore or add additional handling here.
+							break;
+					}
+
+					// Delete the temporary file.
+					File.Delete(tempFilePath);
+				}
+			}
+		}
+
+		private string GetTemporaryDirectory(string guid)
         {
             string tempDirectory = Path.Combine(Path.GetTempPath(), guid);
             Directory.CreateDirectory(tempDirectory);
             return tempDirectory;
         }
 
-        internal void SaveFile(string filename)
-        {
-            using (ZipFile zip = new ZipFile())
-            {
-                foreach (var instance in Instances)
-                {
-                    string dir = GetTemporaryDirectory(instance.Guid);
-                    instance.Markup.SaveToFile(Path.Combine(dir, MarkupFileName));
-                    instance.SnapShotSaveToFile(Path.Combine(dir, SnapshotFileName));
-                    instance.VisualizationInfo.SaveToFile(Path.Combine(dir, ViewpointFileName));
-                    zip.AddDirectory(dir, instance.Guid);
-                }
-                zip.Save(filename);
-                foreach (var instance in Instances)
-                {
-                    Directory.Delete(GetTemporaryDirectory(instance.Guid), true);
-                }
-            }
-        }
-    }
+		internal void SaveFile(string filename)
+		{
+			// Create the zip file for writing.
+			using (FileStream fs = File.Create(filename))
+			using (ZipOutputStream zipStream = new ZipOutputStream(fs))
+			{
+				// Optionally set the compression level (0-9).
+				zipStream.SetLevel(9); // maximum compression
+
+				foreach (var instance in Instances)
+				{
+					string tempDir = GetTemporaryDirectory(instance.Guid);
+					// Save each required file into the temporary directory.
+					instance.Markup.SaveToFile(Path.Combine(tempDir, MarkupFileName));
+					instance.SnapShotSaveToFile(Path.Combine(tempDir, SnapshotFileName));
+					instance.VisualizationInfo.SaveToFile(Path.Combine(tempDir, ViewpointFileName));
+
+					// Add the entire directory into the zip, placing it under a folder named for the instance Guid.
+					AddDirectoryToZip(zipStream, tempDir, instance.Guid);
+				}
+				// Finalize the zip archive.
+				zipStream.Finish();
+			}
+
+			// Remove temporary directories after zipping.
+			foreach (var instance in Instances)
+			{
+				Directory.Delete(GetTemporaryDirectory(instance.Guid), true);
+			}
+		}
+
+		/// <summary>
+		/// Recursively adds all files from the specified folder into the zip archive.
+		/// The files will be placed under a root folder named 'folderEntryName' in the zip.
+		/// </summary>
+		/// <param name="zipStream">The zip output stream.</param>
+		/// <param name="folderPath">The local folder path to add.</param>
+		/// <param name="folderEntryName">The folder name to use inside the zip.</param>
+		private void AddDirectoryToZip(ZipOutputStream zipStream, string folderPath, string folderEntryName)
+		{
+			// Get all files (including in subdirectories).
+			var files = Directory.GetFiles(folderPath, "*", SearchOption.AllDirectories);
+			foreach (var file in files)
+			{
+				// Determine the file's relative path to the folder being zipped.
+				string relativePath = file.Substring(folderPath.Length + 1);
+				// Combine with the folderEntryName to form the full entry name in the zip.
+				string entryName = Path.Combine(folderEntryName, relativePath);
+				// Zip entry names must use forward slashes.
+				entryName = entryName.Replace("\\", "/");
+
+				// Create a new zip entry.
+				ZipEntry entry = new ZipEntry(entryName)
+				{
+					DateTime = File.GetLastWriteTime(file)
+				};
+				zipStream.PutNextEntry(entry);
+
+				// Copy the file into the zip stream.
+				using (FileStream fs = File.OpenRead(file))
+				{
+					fs.CopyTo(zipStream);
+				}
+				zipStream.CloseEntry();
+			}
+		}
+	}
 }
